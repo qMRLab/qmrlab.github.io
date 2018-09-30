04e333a9c3dc7c3581d4483b9feec4d3bceea7ad
Tommy Boshkovski <boshkovski.tommy@gmail.com>
2018-09-29 20:47:16 -0400

    fixed js typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,10 +32,6 @@
 
 	///////////////////////////
 	// Btn nav collapse
-	$('#nav .nav-collapse').ready(function() {
-		if($('#nav').hasClass('open'))
-		$('#nav').removeClass('open');
-	});
 
 	$('#nav .nav-collapse').on('click', function() {
 		$('#nav').toggleClass('open');
